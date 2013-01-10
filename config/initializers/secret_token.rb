f7d30120aefbcdd19bad827c690bab6ab8b89ffa
filmingindex <filmingindex@gmail.com>
2013-01-09 23:02:00 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '4949cc00512ff5f0b8049c6fbbd13a78fcd17efe775f961e0105115733ecca5779db4d09e806b8b5b2a045afe7545e1dc2b1fb6e072d5315cb6fe5c4cf6db615'
